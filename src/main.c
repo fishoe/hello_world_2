b4ff724e48629a361d6276ac1639928aa4948a03
hangil.kim <hangil.kim@stans.co.kr>
2024-08-22 11:29:58 +0900

    changed business logic

--- src/main.c
+++ src/main.c
@@ -3,7 +3,9 @@
 
 int main(void) {
     const int t = sum(4, 7);
+    const int u = sub(8, 3);
 
-    printf("Hello, World! = %d\n", t);
+    printf("4 + 7 = %d\n", t);
+    printf("8 - 3 = %d\n", u);
     return 0;
 }
